4b29f4f31a7b91bb8bd1b688249bd289a59c1b8e
cadenceyeung <55165350+cadenceyeung@users.noreply.github.com>
2020-10-29 12:08:08 -0400

    Initial Commit

---
+++ Aime-Eye/Aime_EyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aime_EyeApp.swift
+//  Aime-Eye
+//
+//  Created by Cadence Yeung on 2020-10-29.
+//
+
+import SwiftUI
+
+@main
+struct Aime_EyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aime-Eye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Aime-Eye
+//
+//  Created by Cadence Yeung on 2020-10-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
